efd4a3b9098264f21598d3686bb517f12e59207f
Amirreza Kadkhodazadeh <amirreza.kadz@gmail.com>
2021-08-31 00:37:02 +0430

    App and index has been modified and assets and layout folders added to project, bootstrap installed also

--- src/App.js
+++ src/App.js
@@ -3,7 +3,31 @@ import React from 'react';
 export default function App() {
     return (
         <>
-            <h1>Hello word</h1>
+            <div className='text-center mt-3'>
+                <h1>
+                    <strong>Eisenhower Matrix</strong>
+                </h1>
+            </div>
+
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-6'>
+                        <div className='box-one'></div>
+                    </div>
+
+                    <div className='col-6'>
+                        <div className='box-two'></div>
+                    </div>
+
+                    <div className='col-6'>
+                        <div className='box-three'></div>
+                    </div>
+
+                    <div className='col-6'>
+                        <div className='box-four'></div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
